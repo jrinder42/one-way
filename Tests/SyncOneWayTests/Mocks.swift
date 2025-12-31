fa55dd97d3629da125dbdc885c3d41f71523a630
Jordan Rinder <jrinder42@users.noreply.github.com>
2025-12-31 12:27:57 -0500

    add RcloneAuth for Oath tests

--- Tests/SyncOneWayTests/Mocks.swift
+++ Tests/SyncOneWayTests/Mocks.swift
@@ -5,18 +5,22 @@ import XCTest
 class MockProcessRunner: ProcessRunnerProtocol {
     var shouldSucceed = true
     var exitStatus: Int32 = 0
+    var stdout = ""
+    var stderr = ""
     
     var executedExecutableURL: URL?
     var executedArguments: [String]?
     
-    func run(executableURL: URL, arguments: [String]) async throws -> Int32 {
+    func run(executableURL: URL, arguments: [String]) async throws -> ProcessResult {
         executedExecutableURL = executableURL
         executedArguments = arguments
         
-        if shouldSucceed {
-            return 0
-        } else {
-            return exitStatus
-        }
+        let status = shouldSucceed ? 0 : exitStatus
+        
+        return ProcessResult(
+            terminationStatus: status,
+            standardOutput: stdout,
+            standardError: stderr
+        )
     }
 }
